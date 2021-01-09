1f751992363edfadbcefde237cc3b1baa362642d
Seth Mitchell <sayotte.mitchell@gmail.com>
2021-01-09 12:09:43 -0500

    TicketData Formatting Shell Complete with Successful API Call & Data Update - DESKTOP ONLY 950px+

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -4,35 +4,9 @@ import axios from "axios";
 
 import TicketHorizontal from './TicketHorizontal';
 import TicketVertical from './TicketVertical';
-
-const options = {
-    method: 'GET',
-    url: 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/CDG-sky/anytime',
-    params: {inboundpartialdate: 'anytime'},
-    headers: {
-      'x-rapidapi-key': process.env.REACT_APP_SKYSCANNER_KEY,
-      'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com'
-    }
-  };
+import BookingData from './BookingData';
 
 const Booking = () => {
-
-    const [samplePrice, setSamplePrice] = useState(null);
-
-    const getFlightData = async () => {
-        axios.request(options)
-        .then(function (response) {
-            setSamplePrice(response.data.Quotes[0].MinPrice);
-            console.log(samplePrice);
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
-      };
-
-    useEffect(() => {
-        getFlightData();
-      });
 
     return (
         <div className="pageContent" id="booking">
@@ -40,16 +14,17 @@ const Booking = () => {
             {(window.innerWidth < 850) ? (
                 <TicketVertical />
             ) : (
-                <div>
-                <div className="ticketContainer">
-                <TicketHorizontal />
-                </div>
-                <div className="renderData">
-                    <h3>Sample Fare</h3>
-                    <h4>{samplePrice}</h4>
-                </div>
+                <div className="ticketRender">
+                    <div className="container">
+                        <TicketHorizontal />
+                    </div>
                 </div>
             )}
+            <div className="data">
+                <div className="dataChild">
+                    <BookingData />
+                </div>
+            </div>
             <div className="bookingControls">
                 <Link to="/confirmation">
                         <button className="buttonMain">Book Now</button>
---
+++ src/components/BookingData.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
+const options = {
+    method: 'GET',
+    url: 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/us/usd/us/sfo/jfk/anytime/anytime',
+    headers: {
+      'x-rapidapi-key': 'dcb1ec75b4mshe35dece3bc5454ep1837eajsnf6a514ee41e9',
+      'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com'
+    }
+  };
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
+
+const BookingData = () => {
+
+    const [fare, setFare] = useState(null);
+    const [destinationCode, setDestinationCode] = useState(null);
+    const [cityName, setCityName] = useState(null);
+    const [countryName, setCountryName] = useState(null);
+    const [outboundDate, setOutboundDate] = useState(null);
+    const [inboundDate, setInboundDate] = useState(null);
+    const [airline, setAirline] = useState(null);
+
+    const getFlightData = async () => {
+        axios.request(options)
+        .then(function (response) {
+            setDestinationCode(response.data.Places[0].IataCode);
+            console.log(destinationCode);
+            setCityName(response.data.Places[0].CityName);
+            console.log(cityName);
+            setCountryName(response.data.Places[0].CountryName);
+            console.log(countryName);
+            setOutboundDate(response.data.Quotes[0].OutboundLeg.DepartureDate);
+            console.log(outboundDate);
+            setInboundDate(response.data.Quotes[0].InboundLeg.DepartureDate);
+            console.log(inboundDate);
+            setAirline(response.data.Carriers[0].Name);
+            console.log(airline);
+            setFare(response.data.Quotes[0].MinPrice);
+            console.log(fare);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+      };
+
+      const dateConversion = (date) => {
+        let str = date;
+        let year = str.slice(0,4);
+        let month = str.slice(5,7);
+        let day = str.slice(8,10);
+        return month + "/" + day + "/" + year
+      }
+
+      console.log(dateConversion(outboundDate));
+
+      let obDate = outboundDate;
+      let obYear = (obDate.slice(0,4));
+      console.log(obDate.slice(5,7));
+      console.log(obDate.slice(8,10));
+
+    useEffect(() => {
+        getFlightData();
+      });
+
+    return (
+        <div className="ticketData">
+        <div className="dataContainerL">
+            <div className="sectionContainer">
+                <div className="numHeader">
+                    <h2>1</h2>
+                    <h3>Destination</h3>
+                </div>
+                <div className="sectionContent">
+                    <p>({destinationCode}) {cityName}, {countryName}</p>
+                </div>
+            </div>
+            <div className="sectionContainer">
+                <div className="numHeader">
+                    <h2>2</h2>
+                    <h3>Dates</h3>
+                </div>
+                <div className="sectionContent">
+                    <p>{dateConversion(outboundDate)} - {dateConversion(inboundDate)}</p>
+                </div>
+            </div>
+            <div className="sectionContainer">
+                <div className="numHeader">
+                    <h2>3</h2>
+                    <h3>Airline</h3>
+                </div>
+                <div className="sectionContent">
+                    <p>{airline}</p>
+                </div>
+            </div>
+        </div>
+        <div className="dataContainerR">
+            <div className="sectionContainer">
+                <div className="numHeader">
+                    <h2>4</h2>
+                    <h3>Itinerary</h3>
+                </div>
+                <div className="sectionContent">
+                    <p>10:28 -- 21:10</p>
+                    <p>07:00 -- 20:49</p>
+                </div>
+            </div>
+            <div className="sectionContainer">
+                <div className="numHeader">
+                    <h2>5</h2>
+                    <h3>Fare</h3>
+                </div>
+                <div className="sectionContent">
+                    <p>As low as:</p>
+                    <p id="farePrice">${fare}<span id="currencyType">usd</span></p>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+}
+
+export default BookingData;
